8fe8a7d071b9675cb649cf03cbf3182890065ccd
lapard1n <rictumarcus@gmail.com>
2022-04-15 23:41:18 +0300

    refactoring slider

--- dist/js/app.js
+++ dist/js/app.js
@@ -168,55 +168,55 @@ function isWebp() {
 }
 isWebp();
 
-// TODO УКАЗАНИЕ СЛАЙДА ПО УМОЛЧАНИЮ
-// TODO КНОПКИ ПРОКРУТКИ
-// TODO ЗАЦИКЛЕННОСТЬ ПЕРЕХОДА
 // TODO АВТО-ПРОКРУТКА СЛАЙДЕРА
-// TODO ПРОКРУТКА СВАЙПОМ
 
 const wrapper = document.querySelector('.slider__wrapper');
-const container = document.querySelector('.slider__container');
 const track = document.querySelector('.slider__track');
 const items = document.querySelectorAll('.slider__item');
 const btnPrev = document.querySelector('.slider__control_prev');
 const btnNext = document.querySelector('.slider__control_next');
 
-function slide(mainWrapper, prev, next) {
+function slide() {
   let posX1 = 0;
   let posX2 = 0;
   let posInitial;
   let posFinal;
   let threshold = 100;
-  let slides = track.querySelectorAll('.slider__item');
-  let slidesLength = slides.length;
-  let slideSize = track.querySelectorAll('.slider__item')[0].offsetWidth;
-  let firstSlide = slides[0];
-  let lastSlide = slides[slidesLength - 1];
+  let slidesLength = items.length;
+  let slideSize = items[0].offsetWidth;
+  let firstSlide = items[0];
+  let lastSlide = items[slidesLength - 1];
   let cloneFirst = firstSlide.cloneNode(true);
   let cloneLast = lastSlide.cloneNode(true);
   let index = 0;
   let allowShift = true;
 
   // ? КЛОНИРУЮ ПЕРВЫЕ И ПОСЛЕДНИЕ СЛАЙДЫ В ОЧЕРЕДЬ
-  track.appendChild(cloneFirst);
-  track.insertBefore(cloneLast, firstSlide);
-  mainWrapper.classList.add('loaded');
+  track.insertBefore(cloneLast, firstSlide); // ДО
+  track.appendChild(cloneFirst); //  ПОСЛЕ
 
-  // ? СОБЫТИЕ МЫШЬЮ
+  // ? КОГДА СЛАЙДЫ КЛАНИРОВАНЫ ПРИМЕНЯЕМ СТИЛИ
+  wrapper.classList.add('loaded');
+
+  // ? СОБЫТИЕ ДЛЯ МЫШИ
   track.onmousedown = dragStart;
 
-  // ? СОБЫТИЯ СВАЙПОМ
+  // ? СОБЫТИЯ ДЛЯ СВАЙПОВ
   track.addEventListener('touchstart', dragStart);
-  track.addEventListener('touchend', dragEnd);
   track.addEventListener('touchmove', dragAction);
+  track.addEventListener('touchend', dragEnd);
 
   // ? СОБЫТИЯ ПО КНОПКАМ
-  prev.addEventListener('click', function () { shiftSlide(-1) })
-  next.addEventListener('click', function () { shiftSlide(1) })
+  btnPrev.addEventListener('click', function () {
+    shiftSlide("prev", "click")
+  })
+  btnNext.addEventListener('click', function () {
+    shiftSlide("next", "click")
+  })
 
   track.addEventListener('transitionend', checkIndex);
 
-  // ! ОПИСЫВАЮ СОБЫТИЯ
+  // TODO ПРОКРУТКА СВАЙПОМ
   function dragStart(e) {
     e = e || window.event;
     e.preventDefault();
@@ -226,8 +226,8 @@ function slide(mainWrapper, prev, next) {
       posX1 = e.touches[0].clientX;
     } else {
       posX1 = e.clientX;
-      document.onmouseup = dragEnd;
       document.onmousemove = dragAction;
+      document.onmouseup = dragEnd;
     }
   }
   function dragAction(e) {
@@ -246,29 +246,31 @@ function slide(mainWrapper, prev, next) {
   function dragEnd(e) {
     posFinal = track.offsetLeft;
     if (posFinal - posInitial < -threshold) {
-      shiftSlide(1, 'drag');
+      shiftSlide("next", "swipe");
     } else if (posFinal - posInitial > threshold) {
-      shiftSlide(-1, 'drag');
+      shiftSlide("prev", "swipe");
     } else {
       track.style.left = (posInitial) + "px";
     }
 
-    document.onmouseup = null;
     document.onmousemove = null;
+    document.onmouseup = null;
   }
 
-  // ? ГЛАВНЫЕ ФУНКЦИИ СМЕЩЕНИЯ
+  // TODO КНОПКИ ПРОКРУТКИ
   function shiftSlide(dir, action) {
     track.classList.add('shifting');
 
     if (allowShift) {
-      if (!action) {
+      // ? УСЛОВИЕ ОБНУЛЕНИЯ НАЧАЛЬНОЙ ПОЗИЦИИ ТОЛЬКО ДЛЯ КНОПОК*
+      if (action == "click") {
         posInitial = track.offsetLeft;
       }
-      if (dir == 1) {
+      // ? УСЛОВИЕ САМОЙ ПРОКРУТКИ
+      if (dir == "next") {
         track.style.left = (posInitial - slideSize) + "px";
         index++;
-      } else if (dir == -1) {
+      } else if (dir == "prev") {
         track.style.left = (posInitial + slideSize) + "px";
         index--;
       }
@@ -277,6 +279,7 @@ function slide(mainWrapper, prev, next) {
     allowShift = false;
   }
 
+  // TODO ЗАЦИКЛЕННОСТЬ ПЕРЕХОДА
   function checkIndex() {
     track.classList.remove('shifting');
 
@@ -294,9 +297,9 @@ function slide(mainWrapper, prev, next) {
   }
 }
 
-slide(wrapper, btnPrev, btnNext);
+slide();
 
-// TODO СОЗДАЮ ИНДИКАТОРЫ ПРОКРУТКИ В ЗАВИСИМОСТИ ОТ КОЛИЧЕСТВА СЛАЙДОВ
+// TODO ИНДИКАТОРЫ ПРОКРУТКИ В ЗАВИСИМОСТИ ОТ КОЛИЧЕСТВА СЛАЙДОВ
 const indicators = document.querySelector('.slider__indicators');
 
 for (let i = 0; i != items.length; i++) {
